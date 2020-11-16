de69354c1ddd6a83461f18e8065d41f7392d3638
Dũng Nguyễn <dung.nguyen@mapmaker.asia>
2020-11-16 14:49:44 +0700

    ,,,...

--- Restaurant/Restaurant/AppContext.cs
+++ Restaurant/Restaurant/AppContext.cs
@@ -1,13 +1,30 @@
-﻿using Restaurant.Models;
+﻿using Acr.UserDialogs;
+using Restaurant.Models;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text;
+using Xamarin.Essentials;
 
 namespace Restaurant
 {
     public class AppContext
     {
+        public AppContext()
+        {
+            Connectivity.ConnectivityChanged += Connectivity_ConnectivityChanged;
+        }
+        void Connectivity_ConnectivityChanged(object sender, ConnectivityChangedEventArgs e)
+        {
+            if (e.NetworkAccess != NetworkAccess.Internet)
+                UserDialogs.Instance.Toast("NetworkWarningMessage");
+        }
+
+
+        ~AppContext()
+        {
+            Connectivity.ConnectivityChanged -= Connectivity_ConnectivityChanged;
+        }
         public Staff CurrentStaff { get; set; } = new Staff();
         public List<Table> Tables = new List<Table>();
         public int KitchenClickCount = 0;
