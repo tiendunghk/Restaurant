1dc911f290d46d896d6e9ac1c1053679c0e02f1f
Dũng Nguyễn <dung.nguyen@mapmaker.asia>
2020-11-16 12:15:57 +0700

    fix role

--- Restaurant/Restaurant/AppShell.xaml.cs
+++ Restaurant/Restaurant/AppShell.xaml.cs
@@ -39,23 +39,25 @@ namespace Restaurant
         }
         protected override async void OnNavigating(ShellNavigatingEventArgs args)
         {
+            var a = CurrentItem;
+            var b = Current;
             //call api to load data here
             //base.OnNavigating(args);
-            if (args.Target.Location.OriginalString.Contains("2"))
+            if (args.Target.Location.OriginalString.Contains("table"))
             {
                 //Table
                 App.Context.TableClickCount++;
                 if (App.Context.TableClickCount <= 1)
                     MessagingCenter.Send("abc", "LoadDataTable");
             }
-            if (args.Target.Location.OriginalString.Contains("4"))
+            if (args.Target.Location.OriginalString.Contains("order"))
             {
                 //order
                 App.Context.OrderClickCount++;
                 if (App.Context.OrderClickCount <= 1)
                     MessagingCenter.Send("abc", "LoadDataOrder");
             }
-            if (args.Target.Location.OriginalString.Contains("6"))
+            if (args.Target.Location.OriginalString.Contains("kitchen"))
             {
                 if (App.Context.CurrentStaff.Role != "Kitchen")
                 {
@@ -71,7 +73,7 @@ namespace Restaurant
                 }
 
             }
-            if (args.Target.Location.OriginalString.Contains("8"))
+            if (args.Target.Location.OriginalString.Contains("manager"))
             {
                 if (App.Context.CurrentStaff.Role != "Manager")
                 {
@@ -84,7 +86,7 @@ namespace Restaurant
                 }
 
             }
-            if (args.Target.Location.OriginalString.Contains("10"))
+            if (args.Target.Location.OriginalString.Contains("report"))
             {
                 if (App.Context.CurrentStaff.Role != "Manager")
                 {
